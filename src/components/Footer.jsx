ef35dabf1856f6bea5d83111f08c7e837d47a362
Evangelos Argyropoulos <vag.argyropoulos@gmail.com>
2025-08-06 23:58:08 +0300

    removed unecessary imports

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
-import { Box, Typography, Link, IconButton } from '@mui/material'
-import InstagramIcon from '@mui/icons-material/Instagram'
-import MailOutlineIcon from '@mui/icons-material/MailOutline'
+import { Box, Typography, IconButton } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 const Footer = () => (
-  <Box sx={{ backgroundColor: '#17313E', color: '#F3E2D4', py: 4, textAlign: 'center' }}>
+  <Box
+    sx={{
+      backgroundColor: "#17313E",
+      color: "#F3E2D4",
+      py: 4,
+      textAlign: "center",
+    }}
+  >
     <Typography variant="body2">
       © {new Date().getFullYear()} Odyssey of Wisdom. All rights reserved.
     </Typography>
@@ -12,19 +19,27 @@ const Footer = () => (
         href="https://www.instagram.com/oddysey_of_wisdom/"
         target="_blank"
         rel="noopener noreferrer"
-        sx={{ color: '#C5B0CD', transition: 'color 0.3s', '&:hover': { color: '#F3E2D4' } }}
+        sx={{
+          color: "#C5B0CD",
+          transition: "color 0.3s",
+          "&:hover": { color: "#F3E2D4" },
+        }}
       >
         <InstagramIcon fontSize="medium" />
       </IconButton>
 
       <IconButton
         href="mailto:odysseyofwisdom.greece@gmail.com"
-        sx={{ color: '#C5B0CD', transition: 'color 0.3s', '&:hover': { color: '#F3E2D4' } }}
+        sx={{
+          color: "#C5B0CD",
+          transition: "color 0.3s",
+          "&:hover": { color: "#F3E2D4" },
+        }}
       >
         <MailOutlineIcon fontSize="medium" />
       </IconButton>
     </Box>
   </Box>
-)
+);
 
-export default Footer
+export default Footer;
