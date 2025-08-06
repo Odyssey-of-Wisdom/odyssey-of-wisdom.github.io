c958e4018666d204856e88b7f4fe06f1ae98e8d5
Evangelos Argyropoulos <vag.argyropoulos@gmail.com>
2025-08-06 23:53:33 +0300

    removed unecessary imports

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,17 +1,16 @@
-import React, { useEffect } from 'react'
-import { Grid, Card, CardContent, CardHeader, Avatar, Typography, Box } from '@mui/material'
-import FormatQuoteIcon from '@mui/icons-material/FormatQuote'
+import React, { useEffect } from "react";
+import { Typography, Box } from "@mui/material";
 
 const Feedback = () => {
   useEffect(() => {
-    const script = document.createElement('script')
-    script.src = 'https://static.elfsight.com/platform/platform.js'
-    script.async = true
-    document.body.appendChild(script)
+    const script = document.createElement("script");
+    script.src = "https://static.elfsight.com/platform/platform.js";
+    script.async = true;
+    document.body.appendChild(script);
     return () => {
-      document.body.removeChild(script)
-    }
-  }, [])
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <Box id="feedback" sx={{ py: 10, px: 4 }}>
@@ -19,14 +18,14 @@ const Feedback = () => {
         What Youth Say
       </Typography>
 
-      <Box sx={{ mb: 8, textAlign: 'center' }}>
+      <Box sx={{ mb: 8, textAlign: "center" }}>
         <div
           className="elfsight-app-98575e3a-92ec-400a-9538-4c4f3948ab14"
           data-elfsight-app-lazy
         ></div>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Feedback
+export default Feedback;
