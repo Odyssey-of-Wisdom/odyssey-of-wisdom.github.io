61f12c3c91397b8abb72edf3bc46c78bb73345d8
Evangelos Argyropoulos <vag.argyropoulos@gmail.com>
2025-08-07 14:06:22 +0300

    added a member

--- src/data/co-founders.jsx
+++ src/data/co-founders.jsx
@@ -1,40 +1,45 @@
-import panagiotisImg from '../assets/panagiotis.jpg'
-import iliasImg from '../assets/ilias.jpg'
-import antonisImg from '../assets/antonis.jpg'
-import giorgosImg from '../assets/giorgos.jpg'
-import vaggelisImg from '../assets/vaggelis.jpg'
-import foteiniImg from '../assets/foteini.png'
+import panagiotisImg from "../assets/panagiotis.jpg";
+import iliasImg from "../assets/ilias.jpg";
+import antonisImg from "../assets/antonis.jpg";
+import giorgosImg from "../assets/giorgos.jpg";
+import vaggelisImg from "../assets/vaggelis.jpg";
+import foteiniImg from "../assets/foteini.png";
+import sotirisImg from "../assets/sotiris.png";
 
 export const cofounders = [
   {
-    name: 'Panagiotis Kotsalis',
+    name: "Panagiotis Kotsalis",
     image: panagiotisImg,
-    bio: `Panagiotis has experience in active citizenship and leadership through youth councils. He promotes civic engagement and intercultural dialogue, inspiring youth with values of unity and cooperation.`
+    bio: `Panagiotis has experience in active citizenship and leadership through youth councils. He promotes civic engagement and intercultural dialogue, inspiring youth with values of unity and cooperation.`,
   },
   {
-    name: 'Ilias Kalantzis',
+    name: "Ilias Kalantzis",
     image: iliasImg,
-    bio: `Ilias is a Business Management graduate pursuing a master's in finance. He has led youth camps, participated in Erasmus+ projects, and received awards for entrepreneurship and presentations.`
+    bio: `Ilias is a Business Management graduate pursuing a master's in finance. He has led youth camps, participated in Erasmus+ projects, and received awards for entrepreneurship and presentations.`,
   },
   {
-    name: 'Antonis Kalantzis',
+    name: "Antonis Kalantzis",
     image: antonisImg,
-    bio: `Antonis is an IT student with experience teaching coding and gaming. With expertise in AI, he co-founded Odyssey of Wisdom to inspire youth through technology and intercultural collaboration.`
+    bio: `Antonis is an IT student with experience teaching coding and gaming. With expertise in AI, he co-founded Odyssey of Wisdom to inspire youth through technology and intercultural collaboration.`,
   },
   {
-    name: 'Giorgos Tsitlakidis',
+    name: "Giorgos Tsitlakidis",
     image: giorgosImg,
-    bio: `Giorgos holds a Mathematics degree and studies Computer Science. After discovering Erasmus+, he developed a passion for youth exchanges and now empowers others as a math teacher and project leader.`
+    bio: `Giorgos holds a Mathematics degree and studies Computer Science. After discovering Erasmus+, he developed a passion for youth exchanges and now empowers others as a math teacher and project leader.`,
   },
   {
-    name: 'Vaggelis Argyropoulos',
+    name: "Vaggelis Argyropoulos",
     image: vaggelisImg,
-    bio: `Vaggelis is a computer scientist interested in how technology affects everyday life. He values learning through travel and cross-cultural exchange, and enjoys being part of different places and communities.`
+    bio: `Vaggelis is a computer scientist interested in how technology affects everyday life. He values learning through travel and cross-cultural exchange, and enjoys being part of different places and communities.`,
   },
   {
-	name: 'Foteini Karamitsou',
-	image: foteiniImg,
-	bio: `Foteini is a special education student, passionate about youth work and storytelling. Curious about cultures, she shares Erasmus+ stories through blogging and social media.`
+    name: "Foteini Karamitsou",
+    image: foteiniImg,
+    bio: `Foteini is a special education student, passionate about youth work and storytelling. Curious about cultures, she shares Erasmus+ stories through blogging and social media.`,
   },
-
-]
+  {
+    name: "Sotiris Kouroukafopoulos",
+    image: sotirisImg,
+    bio: `Sotirios is a 21-year-old Law student from Greece, active in Erasmus+ and volunteering. He's a youth leader with strong teamwork skills and a passion for intercultural learning.`,
+  },
+];
